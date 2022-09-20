3e7f55ab7e077832a59e7aa36619e838b652a61e
cooldj101 <dsavory22@gmail.com>
2022-09-19 21:42:02 -0500

    giving up with this darn it

--- app/models/book.rb
+++
@@ -1,9 +0,0 @@
-class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :release, presence: true
-    validates :price, presence: true
-    
-    has_many :users, through: :user_books
-    has_many :user_books
-end
